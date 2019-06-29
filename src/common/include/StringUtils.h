2611ae46d0333ea7990767477f247c65052758a8
limin <lmyooyo@gmail.com>
2019-06-29 15:41:17 +0800

    Update StringUtils utils.

--- src/common/include/StringUtils.h
+++ src/common/include/StringUtils.h
@@ -12,63 +12,30 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <sstream>
 
 class StringUtils {
 public:
 
-    static std::string trimLeft(const std::string &str, const std::string &token = " ") {
-        std::string t = str;
-        t.erase(0, t.find_first_not_of(token));
-        return t;
-    }
+    static std::string trimLeft(const std::string &str, const std::string &token = " ");
 
-    static std::string trimRight(const std::string &str, const std::string &token = " ") {
-        std::string t = str;
-        t.erase(t.find_last_not_of(token) + 1);
-        return t;
-    }
+    static std::string trimRight(const std::string &str, const std::string &token = " ");
 
-    static std::string trim(const std::string &str, const std::string &token = " ") {
-        std::string t = str;
-        t.erase(0, t.find_first_not_of(token));
-        t.erase(t.find_last_not_of(token) + 1);
-        return t;
-    }
+    static std::string trim(const std::string &str, const std::string &token = " ");
 
-    static std::string toLower(const std::string &str) {
-        std::string t = str;
-        std::transform(t.begin(), t.end(), t.begin(), tolower);
-        return t;
-    }
+    static std::string toLower(const std::string &str);
 
-    static std::string toUpper(const std::string &str) {
-        std::string t = str;
-        std::transform(t.begin(), t.end(), t.begin(), toupper);
-        return t;
-    }
+    static std::string toUpper(const std::string &str);
 
-    static bool startsWith(const std::string &str, const std::string &substr) {
-        return str.find(substr) == 0;
-    }
+    static bool startsWith(const std::string &str, const std::string &substr);
 
-    static bool endsWith(const std::string &str, const std::string &substr) {
-        return str.rfind(substr) == (str.length() - substr.length());
-    }
+    static bool endsWith(const std::string &str, const std::string &substr);
 
-    static bool equalsIgnoreCase(const std::string &str1, const std::string &str2) {
-        return toLower(str1) == toLower(str2);
-    }
+    static bool equalsIgnoreCase(const std::string &str1, const std::string &str2);
 
-    static std::vector<std::string> split(const std::string &str, const std::string &delimiter) {
-        char *save = nullptr;
-        char *token = strtok_r(const_cast<char *>(str.c_str()), delimiter.c_str(), &save);
-        std::vector<std::string> result;
-        while (token != nullptr) {
-            result.emplace_back(token);
-            token = strtok_r(nullptr, delimiter.c_str(), &save);
-        }
-        return result;
-    }
+    static std::vector<std::string> split(const std::string &str, const std::string &delimiter);
+
+    static std::string valueOf(long value);
 };
 
 
