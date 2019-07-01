ac4b66b8b2e3c67d19aa356008ab4ec2e7a9f4e2
李明艺 <lmyooyo@gmail.com>
2019-07-01 21:28:58 +0800

    Update logic.

--- proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/ui/BitmapActivity.kt
+++ proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/ui/BitmapActivity.kt
@@ -22,13 +22,6 @@ class BitmapActivity : BaseActivity() {
         val buf = ByteArray(width * height * 4)
         val ios = FileInputStream(path)
         val len = ios.read(buf)
-        for (i in 0 until height) {
-            for (j in 0 until  width) {
-                val b = buf[i * width * 4 + j * 4]
-                buf[i * width * 4 + j * 4] = buf[i * width * 4 + j * 4 + 3]
-                buf[i * width * 4 + j * 4 + 3] = b
-            }
-        }
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(buf))
         imageView.setImageBitmap(bitmap)
