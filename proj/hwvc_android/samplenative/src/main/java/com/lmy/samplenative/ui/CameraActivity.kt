16f7c534d440b82b1a1cda604d64d4582821018b
李明艺 <lmyooyo@gmail.com>
2019-07-05 11:14:27 +0800

    Support preview after recording.

--- proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/ui/CameraActivity.kt
+++ proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/ui/CameraActivity.kt
@@ -1,13 +1,18 @@
 package com.lmy.samplenative.ui
 
+import android.content.Intent
+import android.net.Uri
 import com.lmy.hwvcnative.processor.HwCameraRecorder
 import com.lmy.samplenative.BaseActivity
 import com.lmy.samplenative.R
+import com.lmy.samplenative.VideoActivity
 import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
 
 class CameraActivity : BaseActivity() {
     private var recorder: HwCameraRecorder? = HwCameraRecorder()
     private var recording = false
+    private var requestPreview = false
 
     override fun getLayoutResource(): Int = R.layout.activity_camera
     override fun initView() {
@@ -24,10 +29,19 @@ class CameraActivity : BaseActivity() {
                 recordBtn.text = "Record"
             }
         }
+        nextBtn.setOnClickListener {
+            requestPreview = true
+            finish()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         recorder?.release()
+        if (requestPreview) {
+            startActivity(Intent(this, VideoActivity::class.java).apply {
+                data = Uri.fromFile(File("/sdcard/hw_encoder.mp4"))
+            })
+        }
     }
 }
