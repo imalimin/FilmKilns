59517735ca6adbdcca547c4a87d70bceaf3ff364
limin <lmyooyo@gmail.com>
2019-07-08 23:22:55 +0800

    Change test file.

--- proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/AudioActivity.kt
+++ proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/AudioActivity.kt
@@ -26,7 +26,7 @@ class AudioActivity : BaseActivity(), SeekBar.OnSeekBarChangeListener {
         if (uri == null)
             uri = intent.getParcelableExtra(Intent.EXTRA_STREAM)
         if (uri == null) {
-            val testFile = File(Environment.getExternalStorageDirectory(), "hw_test.mp4")
+            val testFile = File(externalCacheDir, "/video/hw_small.mp4")
             if (!testFile.exists()) {
                 Toast.makeText(this, "没有找到该文件", Toast.LENGTH_SHORT).show()
                 finish()
