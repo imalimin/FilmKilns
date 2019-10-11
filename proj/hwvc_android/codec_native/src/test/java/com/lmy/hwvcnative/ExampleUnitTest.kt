55840e81d8c39d90a9d208ecf10dc66f01f58f93
李明艺 <lmyooyo@gmail.com>
2019-10-11 11:30:06 +0800

    Support java test & png to base64 string.

---
+++ proj/hwvc_android/codec_native/src/test/java/com/lmy/hwvcnative/ExampleUnitTest.kt
@@ -0,0 +1,31 @@
+package com.lmy.hwvcnative
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.io.FileInputStream
+import java.io.FileWriter
+import java.util.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun png2Base64String() {
+        val pngPath = "/Users/lmy/Filter_04/rise/rise.png"
+        val encoder = Base64.getEncoder()
+        val ins = FileInputStream(pngPath)
+        val result = encoder.encodeToString(ins.readBytes())
+        val writer = FileWriter("$pngPath.txt")
+        writer.write(result)
+        writer.close()
+        ins.close()
+    }
+}
