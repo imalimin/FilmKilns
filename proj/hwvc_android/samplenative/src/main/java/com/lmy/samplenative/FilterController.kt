d6cde1a1ae59ba3e8e71acacb5261ceea6308cdc
李明艺 <lmyooyo@gmail.com>
2019-10-11 16:06:13 +0800

    Update ui.

--- proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/FilterController.kt
+++ proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/FilterController.kt
@@ -4,20 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.os.AsyncTask
 import android.support.v7.app.AlertDialog
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.*
+import android.view.View
+import android.view.ViewGroup
 import android.widget.SeekBar
-import android.widget.TextView
 import com.lmy.hwvcnative.FilterSupport
-import com.lmy.hwvcnative.filter.*
-import com.lmy.samplenative.adapter.OnRecyclerItemClickListener
-import com.lmy.samplenative.adapter.RecyclerAdapter
+import com.lmy.hwvcnative.filter.HwNormalFilter
+import com.lmy.hwvcnative.filter.HwvcFilter
+import com.microsoft.officeuifabric.bottomsheet.BottomSheetDialog
+import com.microsoft.officeuifabric.bottomsheet.BottomSheetItem
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
-import java.lang.Exception
 import java.util.*
 
 /**
@@ -25,7 +23,7 @@ import java.util.*
  */
 class FilterController(private val filterSupport: FilterSupport,
                        private val progressLayout: ViewGroup)
-    : SeekBar.OnSeekBarChangeListener, OnRecyclerItemClickListener.OnItemClickListener {
+    : SeekBar.OnSeekBarChangeListener, BottomSheetItem.OnClickListener {
 
     companion object {
         private val FILTERS = arrayOf(
@@ -36,10 +34,10 @@ class FilterController(private val filterSupport: FilterSupport,
     private var twoBar: SeekBar = progressLayout.getChildAt(1) as SeekBar
     private var thBar: SeekBar = progressLayout.getChildAt(2) as SeekBar
     private var fBar: SeekBar = progressLayout.getChildAt(3) as SeekBar
-    private var dialog: AlertDialog? = null
+    private var bottomSheetDialog: BottomSheetDialog? = null
     private var initDialog: AlertDialog? = null
     private var task: AsyncTask<Void, Void, Void?>? = null
-    private lateinit var hwfDir: String
+    private var hwfDir: String = "${progressLayout.context.externalCacheDir!!.path}/hwf"
 
     init {
         oneBar.setOnSeekBarChangeListener(this)
@@ -65,27 +63,14 @@ class FilterController(private val filterSupport: FilterSupport,
         task?.execute()
     }
 
-    private fun createView(): View {
-        hwfDir = "${progressLayout.context.externalCacheDir.path}/hwf"
-        val layout = LayoutInflater.from(progressLayout.context).inflate(R.layout.dialog_filter, null)
-        val recyclerView = layout.findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context,
-                LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.adapter = Adapter().apply {
-            bindData(FilterItem.from(FILTERS))
-        }
-        recyclerView.addOnItemTouchListener(OnRecyclerItemClickListener(progressLayout.context, this))
-        return layout
-    }
-
     fun chooseFilter(context: Context) {
-        if (null != dialog && dialog!!.isShowing) dialog?.dismiss()
-        dialog = AlertDialog.Builder(context, R.style.BaseAlertDialog_Bottom_Wide)
-                .setView(createView())
-                .create()
-        dialog?.window?.setGravity(Gravity.BOTTOM)
-        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
-        dialog?.show()
+        val list = ArrayList<BottomSheetItem>()
+        FILTERS.forEachIndexed { index, it ->
+            list.add(BottomSheetItem(index, R.mipmap.ic_launcher, it))
+        }
+        bottomSheetDialog = BottomSheetDialog(context, list)
+        bottomSheetDialog?.onItemClickListener = this
+        bottomSheetDialog?.show()
     }
 
     override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
@@ -100,9 +85,8 @@ class FilterController(private val filterSupport: FilterSupport,
         fBar.visibility = if (count > 3) View.VISIBLE else View.GONE
     }
 
-    override fun onItemClick(parent: RecyclerView?, view: View?, position: Int) {
-        dialog?.dismiss()
-        choose(position)
+    override fun onBottomSheetItemClick(item: BottomSheetItem) {
+        choose(item.id)
     }
 
     private fun choose(which: Int) {
@@ -159,10 +143,10 @@ class FilterController(private val filterSupport: FilterSupport,
     private fun initResources() {
         Log.i("HWVC", "initResources")
         val srcHwfDir = "hwf"
-        val destHwfDir = "${progressLayout.context.externalCacheDir.path}/$srcHwfDir"
+        val destHwfDir = "${progressLayout.context.externalCacheDir!!.path}/$srcHwfDir"
         copyAssets(srcHwfDir, destHwfDir)
         val srcImageDir = "image"
-        val destImageDir = "${progressLayout.context.externalCacheDir.path}/$srcImageDir"
+        val destImageDir = "${progressLayout.context.externalCacheDir!!.path}/$srcImageDir"
         copyAssets(srcImageDir, destImageDir)
     }
 
@@ -170,7 +154,7 @@ class FilterController(private val filterSupport: FilterSupport,
         if (!File(destCacheDir).exists()) {
             File(destCacheDir).mkdirs()
         }
-        progressLayout.context.assets.list(srcAsset).forEach {
+        progressLayout.context.assets.list(srcAsset)?.forEach {
             val destFile = File("$destCacheDir/$it")
             if (!destFile.exists()) {
                 var ins: InputStream? = null
@@ -198,41 +182,5 @@ class FilterController(private val filterSupport: FilterSupport,
             }
         }
 
-    }
-
-    private class Adapter : RecyclerAdapter<FilterItem, ViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_filter, null))
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder?, item: FilterItem?, position: Int) {
-            holder?.onBind(item, position)
-        }
-
-    }
-
-    private class ViewHolder(itemView: View) : RecyclerAdapter.BaseViewHolder<FilterItem>(itemView) {
-        private val nameView: TextView = itemView.findViewById(R.id.name)
-        override fun onBind(item: FilterItem?, position: Int) {
-            if (null == item) {
-                nameView.text = "Unknown"
-                return
-            }
-            nameView.text = item.name
-        }
-
-    }
-
-    private data class FilterItem(var name: String,
-                                  var clazz: Class<Any>?) {
-        companion object {
-            fun from(array: Array<String>): List<FilterItem> {
-                val list = ArrayList<FilterItem>()
-                array.forEach {
-                    list.add(FilterItem(it, null))
-                }
-                return list
-            }
-        }
     }
 }
