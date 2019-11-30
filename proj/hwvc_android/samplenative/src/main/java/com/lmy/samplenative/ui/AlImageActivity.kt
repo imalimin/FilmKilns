05d6a3221e13fee6b9d2df35d9ddeabb67da0801
李明艺 <lmyooyo@gmail.com>
2019-11-30 23:07:23 +0800

    Update ui.

--- proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/ui/AlImageActivity.kt
+++ proj/hwvc_android/samplenative/src/main/java/com/lmy/samplenative/ui/AlImageActivity.kt
@@ -2,20 +2,23 @@ package com.lmy.samplenative.ui
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Environment
 import android.util.Log
 import android.view.KeyEvent
 import android.view.SurfaceHolder
 import android.view.View
 import android.widget.SeekBar
 import com.lmy.common.ui.GallerySelectActivity
+import com.lmy.hwvcnative.entity.AlResult
 import com.lmy.hwvcnative.processor.AlImageProcessor
 import com.lmy.samplenative.BaseActivity
 import com.lmy.samplenative.R
+import com.microsoft.officeuifabric.bottomsheet.BottomSheetDialog
+import com.microsoft.officeuifabric.bottomsheet.BottomSheetItem
 import kotlinx.android.synthetic.main.activity_al_image.*
-import java.io.File
 
-class AlImageActivity : BaseActivity(), SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+class AlImageActivity : BaseActivity(), SeekBar.OnSeekBarChangeListener, View.OnClickListener,
+        BottomSheetItem.OnClickListener {
+    private var bottomSheetDialog: BottomSheetDialog? = null
     private var processor: AlImageProcessor? = null
     private val mLayers = ArrayList<Int>()
     private var mCurrentLayer = -1
@@ -39,7 +42,7 @@ class AlImageActivity : BaseActivity(), SeekBar.OnSeekBarChangeListener, View.On
         surfaceView.keepScreenOn = true
         surfaceView.holder.addCallback(surfaceCallback)
         GallerySelectActivity.request(this, 100, 1)
-        addLayerBtn.setOnClickListener(this)
+        showOpt.setOnClickListener(this)
         scaleBar.setOnSeekBarChangeListener(this)
         transBar.setOnSeekBarChangeListener(this)
         processor = lastCustomNonConfigurationInstance as AlImageProcessor?
@@ -51,9 +54,8 @@ class AlImageActivity : BaseActivity(), SeekBar.OnSeekBarChangeListener, View.On
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.addLayerBtn -> {
-                GallerySelectActivity.request(this, 100, 1)
-//                processor?.removeLayer(mCurrentLayer)
+            R.id.showOpt -> {
+                showAllOpt()
             }
         }
     }
@@ -108,5 +110,44 @@ class AlImageActivity : BaseActivity(), SeekBar.OnSeekBarChangeListener, View.On
                 }
             }
         }
+    }
+
+    private val OPTS = arrayListOf<BottomSheetItem>(
+            BottomSheetItem(0, R.mipmap.ic_launcher, "Add Layer"),
+            BottomSheetItem(1, R.mipmap.ic_launcher, "Show All Layer"))
+
+    override fun onBottomSheetItemClick(item: BottomSheetItem) {
+        when (item.id) {
+            0 -> {
+                GallerySelectActivity.request(this, 100, 1)
+            }
+            1 -> {
+                showAllLayers()
+            }
+        }
+    }
+
+    private fun showAllOpt() {
+        bottomSheetDialog = BottomSheetDialog(this, OPTS)
+        bottomSheetDialog?.onItemClickListener = this
+        bottomSheetDialog?.show()
+    }
+
+    private fun showAllLayers() {
+        val list = mLayers.mapTo(ArrayList()) { BottomSheetItem(it, R.mipmap.ic_launcher, "Layer $it") }
+        val dialog = BottomSheetDialog(this, list)
+        dialog.onItemClickListener = object : BottomSheetItem.OnClickListener {
+            override fun onBottomSheetItemClick(item: BottomSheetItem) {
+                if (AlResult.SUCCESS == processor?.removeLayer(item.id)) {
+                    for (i in 0 until mLayers.size) {
+                        if (mLayers[i] == item.id) {
+                            mLayers.removeAt(i)
+                        }
+
+                    }
+                }
+            }
+        }
+        dialog.show()
     }
 }
