ddf01a4e6f903552f5e49297066cb0bd61457abf
李明艺 <lmyooyo@gmail.com>
2019-12-16 10:22:26 +0800

    Fix crop.

--- src/render/image/model/AlCropOperateModel.cpp
+++ src/render/image/model/AlCropOperateModel.cpp
@@ -44,10 +44,8 @@ HwResult AlCropOperateModel::measure(AlAbsOperateModel::AlLayerDesc desc,
     AlOrthMatrix oMat;
     oMat.update(destRectF.left, destRectF.right, destRectF.bottom, destRectF.top, -1.0f, 1.0f);
     AlMatrix tMat;
-//    t1Mat.setTranslate(-0.5f, -0.5f);
     tMat.setRotation(AlMath::PI * rotation.toFloat());
-    tMat.setScale(rectF.getHeight() / 2.0f, rectF.getHeight() / 2.0f);
-//    t2Mat.setTranslate(0.5f, 0.5f);
+//    tMat.setScale(rectF.getHeight() / 2.0f, rectF.getHeight() / 2.0f);
     AlMatrix mat = oMat * tMat;
     AlVec4 lt(srcRectF.left, srcRectF.top);
     AlVec4 lb(srcRectF.left, srcRectF.bottom);
@@ -80,19 +78,8 @@ void AlCropOperateModel::_calculateRect(AlSize &src, AlSize &dest,
         destRectF.bottom = -aspectRatio;
         destRectF.top = -destRectF.bottom;
     }
-    /// 根据Canvas大小计算纹理顶点
-    /// 保证图片总是完整填充到Canvas
-    /// 并保证至Layer和Canvas至少有一边相等
-    /// 此时layer model的scale=1为默认状态
-    if (src.width / (float) src.height > dest.width / (float) dest.height) {
-        srcRectF.left = destRectF.left;
-        srcRectF.right = -srcRectF.left;
-        srcRectF.bottom = srcRectF.left * src.height / (float) src.width;
-        srcRectF.top = -srcRectF.bottom;
-    } else {
-        srcRectF.bottom = destRectF.bottom;
-        srcRectF.top = -srcRectF.bottom;
-        srcRectF.left = srcRectF.bottom * src.width / (float) src.height;
-        srcRectF.right = -srcRectF.left;
-    }
+    srcRectF.left = destRectF.left * rectF.getWidth() / 2.0f;
+    srcRectF.right = -srcRectF.left;
+    srcRectF.bottom = destRectF.bottom * rectF.getHeight() / 2.0f;
+    srcRectF.top = -srcRectF.bottom;
 }
