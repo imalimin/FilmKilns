676d68d380205acee4845cfbe0c2d2f4404b7d66
Alimin <aliminabc@gmail.com>
2020-01-05 17:36:29 +0800

    Add ios demo.

---
+++ proj/hwvc_ios/AlHwvcDemo/AlHwvcDemo/ui/main/MainController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AlHwvcDemo
+//
+//  Created by lmy on 2020/1/5.
+//  Copyright © 2020 Alimin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
