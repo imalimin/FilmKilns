9ae567d8b4fbe4f6e2b97c20ccba15bc517639fa
limin <lmyooyo@gmail.com>
2020-04-12 09:13:10 +0800

    Add init repos shell.

---
+++ proj/hwvc_android/init-repo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git subtree add --prefix=hwcommon git@github.com:imalimin/hwcommon.git master --squash
+git subtree add --prefix=hwmvp git@github.com:imalimin/hwmvp.git master --squash
+git subtree add --prefix=AlFile git@github.com:imalimin/AlFile.git master --squash
