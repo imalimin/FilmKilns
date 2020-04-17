e192983ed6c8a5096d5be94196668954344490f6
Alimin <aliminabc@gmail.com>
2020-04-17 20:32:13 +0800

    Try support graphic core

---
+++ src/al_graphic/include/AlAbsGraphicPipe.h
@@ -0,0 +1,17 @@
+/*
+* Copyright (c) 2018-present, aliminabc@gmail.com.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+#ifndef HWVC_ANDROID_ALABSGRAPHICPIPE_H
+#define HWVC_ANDROID_ALABSGRAPHICPIPE_H
+
+
+class AlAbsGraphicPipe {
+
+};
+
+
+#endif //HWVC_ANDROID_ALABSGRAPHICPIPE_H
---
+++ src/al_graphic/include/AlGLGraphicPipe.h
@@ -0,0 +1,17 @@
+/*
+* Copyright (c) 2018-present, aliminabc@gmail.com.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+#ifndef HWVC_ANDROID_ALGLGRAPHICPIPE_H
+#define HWVC_ANDROID_ALGLGRAPHICPIPE_H
+
+
+class AlGLGraphicPipe {
+
+};
+
+
+#endif //HWVC_ANDROID_ALGLGRAPHICPIPE_H
