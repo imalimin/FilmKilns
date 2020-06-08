a62f4d28e67f84d9546ee1e5b6c3457e697981a0
Alimin <aliminabc@gmail.com>
2020-06-08 18:02:16 +0800

    New bundle.

---
+++ src/al_common/entity/AlBundle.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2018-present, aliminabc@gmail.com.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "AlBundle.h"
+#include "AlString.h"
+
+#define PUT_PRI(_pri) \
+if (map.end() != map.find(key)) { \
+  return false; \
+} \
+_put(key, new _pri(val)); \
+
+#define GET_PRI(_pri) \
+auto itr = map.find(key); \
+if (map.end() != itr && itr->second) { \
+  _pri *val = dynamic_cast<_pri *>(itr->second); \
+  if (val) { \
+    return val->value(); \
+  } \
+} \
+
+AlBundle::AlBundle() : Object() {
+
+}
+
+AlBundle::AlBundle(const AlBundle &o) : Object() {
+
+}
+
+AlBundle::~AlBundle() {
+    auto itr = map.begin();
+    while (map.end() != itr) {
+        delete itr->second;
+    }
+    map.clear();
+}
+
+void AlBundle::_put(std::string &key, Object *val) {
+    map.insert(make_pair(key, val));
+}
+
+bool AlBundle::put(std::string key, int32_t val) {
+    PUT_PRI(AlInteger)
+    return true;
+}
+
+bool AlBundle::put(std::string key, int64_t val) {
+    PUT_PRI(AlLong)
+    return true;
+}
+
+bool AlBundle::put(std::string key, float val) {
+    PUT_PRI(AlFloat)
+    return true;
+}
+
+bool AlBundle::put(std::string key, double val) {
+    PUT_PRI(AlDouble)
+    return true;
+}
+
+bool AlBundle::put(std::string key, uint8_t val) {
+    PUT_PRI(AlByte)
+    return true;
+}
+
+bool AlBundle::put(std::string key, char val) {
+    PUT_PRI(AlChar)
+    return true;
+}
+
+bool AlBundle::put(std::string key, std::string val) {
+    if (map.end() != map.find(key)) {
+        return false;
+    }
+    _put(key, new AlString(val));
+    return true;
+}
+
+int32_t AlBundle::get(std::string key, int32_t def) {
+    GET_PRI(AlInteger)
+    return def;
+}
+
+int64_t AlBundle::get(std::string key, int64_t def) {
+    GET_PRI(AlLong)
+    return def;
+}
+
+float AlBundle::get(std::string key, float def) {
+    GET_PRI(AlFloat)
+    return def;
+}
+
+double AlBundle::get(std::string key, double def) {
+    GET_PRI(AlDouble)
+    return def;
+}
+
+uint8_t AlBundle::get(std::string key, uint8_t def) {
+    GET_PRI(AlByte)
+    return def;
+}
+
+char AlBundle::get(std::string key, char def) {
+    GET_PRI(AlChar)
+    return def;
+}
+
+std::string AlBundle::get(std::string key, std::string def) {
+    auto itr = map.find(key);
+    if (map.end() != itr && itr->second) {
+        AlString *val = dynamic_cast<AlString *>(itr->second);
+        if (val) {
+            return val->str();
+        }
+    }
+    return def;
+}
+
+void AlBundle::remove(std::string key) {
+    auto itr = map.find(key);
+    if (map.end() != itr) {
+        delete itr->second;
+        map.erase(itr);
+    }
+}
---
+++ src/al_common/include/AlBundle.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018-present, aliminabc@gmail.com.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef HWVC_ANDROID_ALBUNDLE_H
+#define HWVC_ANDROID_ALBUNDLE_H
+
+#include "AlPrimitiveType.h"
+#include <map>
+
+AL_CLASS AlBundle AL_EXTEND Object {
+public:
+    AlBundle();
+
+    AlBundle(const AlBundle &o);
+
+    virtual ~AlBundle();
+
+    bool put(std::string key, int32_t val);
+
+    bool put(std::string key, int64_t val);
+
+    bool put(std::string key, float val);
+
+    bool put(std::string key, double val);
+
+    bool put(std::string key, uint8_t val);
+
+    bool put(std::string key, char val);
+
+    bool put(std::string key, std::string val);
+
+    int32_t get(std::string key, int32_t def);
+
+    int64_t get(std::string key, int64_t def);
+
+    float get(std::string key, float def);
+
+    double get(std::string key, double def);
+
+    uint8_t get(std::string key, uint8_t def);
+
+    char get(std::string key, char def);
+
+    std::string get(std::string key, std::string def);
+
+    void remove(std::string key);
+
+private:
+    void _put(std::string &key, Object *val);
+
+private:
+    std::map<std::string, Object *> map;
+};
+
+
+#endif //HWVC_ANDROID_ALBUNDLE_H
