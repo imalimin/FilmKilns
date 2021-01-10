7578dce53674f525f7e0089a119b3bd780146e03
Alimin <aliminabc@gmail.com>
2021-01-10 22:01:31 +0800

    Import sk smart pointer.

---
+++ proj/FkAndroid/fkandroid/src/test/cpp/samples/FkSPObject.cpp
@@ -0,0 +1,25 @@
+/*
+* Copyright (c) 2018-present, aliminabc@gmail.com.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+#include "FkSPObject.h"
+#include "FkDefinition.h"
+
+FkSPObject::FkSPObject() {
+    FkLogI("FkTest", "FkSPObject");
+}
+
+FkSPObject::~FkSPObject() {
+    FkLogI("FkTest", "~FkSPObject");
+}
+
+FkSPObject2::FkSPObject2() {
+    FkLogI("FkTest", "FkSPObject2");
+}
+
+FkSPObject2::~FkSPObject2() {
+    FkLogI("FkTest", "~FkSPObject2");
+}
