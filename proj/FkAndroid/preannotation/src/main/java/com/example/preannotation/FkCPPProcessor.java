0c6e64815b7a14e1bfa795833f554e4b135338fe
alimin <aliminabc@gmail.com>
2021-05-31 09:03:12 +0800

    Support pre annotation

---
+++ proj/FkAndroid/preannotation/src/main/java/com/example/preannotation/FkCPPProcessor.java
@@ -0,0 +1,8 @@
+package com.example.preannotation;
+
+import com.google.auto.service.AutoService;
+//import javax.annotation.processing.AbstractProcessor;
+
+@AutoService(Process.class)
+public class FkCPPProcessor {
+}
