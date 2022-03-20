604c05769a2167da8922f1c6764542c4b834ed4f
alimin <aliminabc@gmail.com>
2022-03-20 22:51:44 +0800

    Add cover for test.

--- proj/FkAndroid/fkapp/src/main/java/com/alimin/fk/app/widget/PointView.kt
+++ proj/FkAndroid/fkapp/src/main/java/com/alimin/fk/app/widget/PointView.kt
@@ -10,6 +10,7 @@ import android.view.View
 
 class PointView : View {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val point = Point(0, 0)
 
     constructor(context: Context) : super(context) {
@@ -34,10 +35,20 @@ class PointView : View {
     private fun initialize() {
         paint.color = Color.BLACK
         paint.strokeWidth = 15f
+        linePaint.color = Color.BLACK
+        linePaint.strokeWidth = 1f
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.drawLine(
+            0.0f, measuredHeight / 2.0f,
+            measuredWidth.toFloat(), measuredHeight / 2.0f, linePaint
+        )
+        canvas?.drawLine(
+            measuredWidth / 2.0f, 0.0f,
+            measuredWidth / 2.0f, measuredHeight.toFloat(), linePaint
+        )
         canvas?.drawPoint(point.x.toFloat(), point.y.toFloat(), paint)
     }
 
