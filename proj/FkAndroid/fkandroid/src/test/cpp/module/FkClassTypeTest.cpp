fbda165bc6f4e5ddba53141c0260f6dab04be0ec
alimin <aliminabc@gmail.com>
2022-04-30 00:37:24 +0800

    Other class type.

--- proj/FkAndroid/fkandroid/src/test/cpp/module/FkClassTypeTest.cpp
+++ proj/FkAndroid/fkandroid/src/test/cpp/module/FkClassTypeTest.cpp
@@ -35,24 +35,84 @@ TEST(FkClassTypeTests, Instance) {
 //    EXPECT_TRUE(FK_INSTANCE_OF(prt, FkObject));
 }
 
+class FkClassHierarchy {
+public:
+    FkClassHierarchy(const char *name) : name(name), hashValue() {
+        id = hashValue(name);
+    }
+
+    FkClassHierarchy(const char *name, const FkClassHierarchy &o) : name(name), hashValue() {
+        id = hashValue(name);
+        next = &o;
+    }
+
+    FkClassHierarchy(const FkClassHierarchy &o) = delete;
+
+    virtual ~FkClassHierarchy() {}
+
+    bool is(const FkClassHierarchy &o) const {
+        if (this == &o) {
+            return true;
+        }
+        auto cur = const_cast<FkClassHierarchy *>(this);
+        do {
+            if (cur->id == o.id) {
+                return true;
+            }
+        } while ((cur = const_cast<FkClassHierarchy *>(cur->next)) != nullptr);
+        return false;
+    }
+
+private:
+    size_t id;
+    const char *name;
+    const std::hash<std::string> hashValue;
+    const FkClassHierarchy *next = nullptr;
+};
+
+#define FK_CLASS_DEF(NAME, SUPER) \
+namespace NAME ## _Info { \
+const char *name = #NAME; \
+FkClassHierarchy hierarchy(name, SUPER ## _Info::hierarchy); \
+} \
+class NAME : public SUPER         \
+
+namespace FkObjectA_Info {
+    const char *name = "FkObjectA";
+    FkClassHierarchy hierarchy(name);
+}
+
 class FkObjectA : public FkObject {
 public:
+    virtual const FkClassHierarchy &classType() {
+        return FkObjectA_Info::hierarchy;
+    }
+
+public:
     FkObjectA() {
-        FK_MARK_SUPER
     };
 };
 
-class FkObjectB : public FkObjectA {
+FK_CLASS_DEF(FkObjectB, FkObjectA) {
 public:
-    FkObjectB() : classGuard(clsType) {
-    };
-    const FkClassTypeGuard<FkObjectB> classGuard;
+    virtual const FkClassHierarchy &classType() override {
+        return FkObjectB_Info::hierarchy;
+    }
+
+public:
+    FkObjectB() : FkObjectA() {
+    }
 };
-class FkObjectC : public FkObject {
+
+FK_CLASS_DEF(FkObjectC, FkObjectB) {
 public:
-    FkObjectC() : classGuard(clsType) {
-    };
-    const FkClassTypeGuard<FkObjectC> classGuard;
+    virtual const FkClassHierarchy &classType() override {
+        return FkObjectC_Info::hierarchy;
+    }
+
+public:
+    FkObjectC() : FkObjectB() {
+    }
 };
 
 TEST(FkClassHierarchyTest, hierarchy2) {
@@ -60,10 +120,21 @@ TEST(FkClassHierarchyTest, hierarchy2) {
     auto b = std::make_shared<FkObjectB>();
     auto c = std::make_shared<FkObjectC>();
     std::shared_ptr<FkObjectA> d = b;
-    EXPECT_TRUE(FK_CLASS_TYPE_EQUALS2(b, a));
-    EXPECT_FALSE(FK_CLASS_TYPE_EQUALS2(a, b));
-    EXPECT_FALSE(FK_CLASS_TYPE_EQUALS2(b, c));
-    EXPECT_TRUE(FK_CLASS_TYPE_EQUALS2(d, b));
+
+    EXPECT_TRUE(a->classType().is(FkObjectA_Info::hierarchy));
+    EXPECT_FALSE(a->classType().is(FkObjectB_Info::hierarchy));
+
+    EXPECT_TRUE(b->classType().is(FkObjectA_Info::hierarchy));
+    EXPECT_TRUE(b->classType().is(FkObjectB_Info::hierarchy));
+    EXPECT_FALSE(b->classType().is(FkObjectC_Info::hierarchy));
+
+    EXPECT_TRUE(d->classType().is(FkObjectA_Info::hierarchy));
+    EXPECT_TRUE(d->classType().is(FkObjectB_Info::hierarchy));
+    EXPECT_FALSE(d->classType().is(FkObjectC_Info::hierarchy));
+
+    EXPECT_TRUE(d->classType().is(a->classType()));
+    EXPECT_TRUE(d->classType().is(b->classType()));
+    EXPECT_FALSE(d->classType().is(c->classType()));
 }
 
 //TEST(FkClassTypeTests, SuperTrack) {
